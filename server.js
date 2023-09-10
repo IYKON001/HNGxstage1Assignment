2009868e9904754c0e42befea3add38cd57b0796
IYKON <121290225+IYKON001@users.noreply.github.com>
2023-09-10 16:05:44 +0100

    changes made

--- server.js
+++ server.js
@@ -4,37 +4,37 @@ const server = express();
 
 const PORT = 3000;
 
-// Manually set values for IYKON and BACKEND
-const Slack_Name = "IYKON";
-const Track = "BACKEND";
-
 server.get('/get_time', (req,res) =>{
-    // Use the manually set values
-    const currentDay = new Date().toLocaleDateString('en-US', {weekday: 'long'})
+    const { Slack_Name, Track } = req.query;
+
+    //get current day of the week
+    const currentDay = new Date().toLocaleDateString('en-US', {weekday: 'long'});
     const currentUtcTime = new Date().toUTCString();
 
+    //return info in json format
     const response = {
         Slack_Name,
         current_day: currentDay,
         current_utc_time: currentUtcTime,
         Track,
-    }
+    };
 
     res.json(response);
-})
+});
 
 server.get('/github_info', (req,res) =>{
 
-    const githubUrlFolder = "https://github.com/IYKON001/HNGxstage1Assignment"
-    const githubUrlSource = "https://github.com/IYKON001/HNGxstage1Assignment/blob/main/server.js"
+    //get github url's
+    const githubUrlFile = "https://github.com/IYKON001/HNGxstage1Assignment/blob/main/server.js";
+    const githubUrlSource = "https://github.com/IYKON001/HNGxstage1Assignment";
 
     const response ={
-        github_url_folder: githubUrlFolder,
+        github_url_file: githubUrlFile,
         github_url_source: githubUrlSource
-    }
-    res.json(response)
-})
+    };
+    res.json(response);
+});
 
 server.listen(PORT,() => {
     console.log(`Server is running at http://localhost:${PORT}`);
-})
+});
