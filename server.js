b246562fdd730ba12e6c1a33139cf50cdd065229
IYKON <121290225+IYKON001@users.noreply.github.com>
2023-09-10 21:08:27 +0100

    updated

--- server.js
+++ server.js
@@ -4,31 +4,31 @@ const server = express();
 
 const PORT = 3000;
 
-server.get('/get_time', (req,res) =>{
-    const { Slack_Name, Track } = req.query;
-
-    //get current day of the week
-    const currentDay = new Date().toLocaleDateString('en-US', {weekday: 'long'});
-    const currentUtcTime = new Date().toUTCString();
-
-    //return info in json format
+server.get('/', (req,res) =>{
+   
     const response = {
-        Slack_Name,
-        current_day: currentDay,
-        current_utc_time: currentUtcTime,
-        Track,
+      message: "good"
     };
 
     res.json(response);
 });
 
-server.get('/github_info', (req,res) =>{
+server.get('/api', (req,res) =>{
+    const { slack_name, track } = req.query;
+
+    const currentDay = new Date().toLocaleDateString('en-NG', { weekday: 'short'});
+    const currentUtcTime = new Date().toISOString();
 
     //get github url's
     const githubUrlFile = "https://github.com/IYKON001/HNGxstage1Assignment/blob/main/server.js";
     const githubUrlSource = "https://github.com/IYKON001/HNGxstage1Assignment";
 
     const response ={
+        slack_name,
+        current_day: currentDay,
+        current_utc_time: currentUtcTime,
+        track,
+        status_code: 200,
         github_url_file: githubUrlFile,
         github_url_source: githubUrlSource
     };
